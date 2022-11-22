e42c408c596c055dbb2845b66c0e70e716a9156d
chemburleno <chemburleno@yandex.ru>
2022-11-22 14:15:53 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,33 @@
+import Swiper, { Navigation, Pagination, EffectFade } from 'swiper';
+
+const swiper1 = new Swiper('.swiper1', {
+  modules: [Navigation, Pagination, EffectFade],
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction'
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  allowTouchMove: false,
+  spaceBetween: 10,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  }
+});
+
+const calcNews = () => {
+  const containerLandmark = document.querySelector('.container-landmark').offsetHeight;
+  const containerMainNews = document.querySelector('.container-main-news');
+  let containerNews = containerMainNews.querySelectorAll('.container-news');
+  containerMainNews.classList.remove('container_display-none');
+
+  while (containerMainNews.offsetHeight + 555 > containerLandmark) {
+    containerNews[containerNews.length - 1].remove();
+    containerNews = containerMainNews.querySelectorAll('.container-news');
+  }
+}
+
+calcNews();

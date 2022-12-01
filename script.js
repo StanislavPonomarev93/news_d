5dcffc045d8321668e3cd05ca89a634980d598d7
chemburleno <chemburleno@yandex.ru>
2022-12-01 11:19:50 +0300

    Updates

---
+++ script.js
@@ -0,0 +1,34 @@
+const swiper1 = new Swiper('.swiper1', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction'
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  allowTouchMove: false,
+  spaceBetween: 10,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  }
+});
+
+const calcNews = () => {
+  const containerLandmark = document.querySelector('.container-landmark').clientHeight;
+  const containerAdaptive = document.querySelector('.container-adaptive');
+  const containerMainNews = document.querySelector('.container-main-news');
+  let containerNews = containerMainNews.querySelectorAll('.container-news');
+  containerMainNews.classList.remove('container_display-none');
+
+  while (containerLandmark < containerAdaptive.clientHeight) {
+    containerNews[containerNews.length - 1].remove();
+    containerNews = containerMainNews.querySelectorAll('.container-news');
+  }
+}
+
+if (document.querySelector('.container-landmark')) {
+  calcNews();
+}
+
